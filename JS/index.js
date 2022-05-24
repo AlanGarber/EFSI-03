fbb134f9a86233f1d9dfe79825234c69251d36f7
SANTIAGO AGUSTIN <46123578@ort>
2022-05-24 15:57:39 -0300

    ok

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,4 @@
-let arrayTareasIniciadas=[];
-let arrayTareasFinalizadas=[];
+let arrayTareas=[];
 let ultimo = 0;
 
 let deployForm=()=>{
@@ -11,20 +10,38 @@ let hideForm=()=>{
 }
 
 let AgregarTarea=(titulo)=> {
-    arrayTareasIniciadas.push(titulo);
-    let ul=document.getElementById("tareasIniciadas")
-    ul.innerHTML+=`<li><input type="checkbox" id="${ultimo}" onclick=Marcar(id)> ${titulo}</input></li>`
+    let tarea={
+        id: ultimo,
+        titulo:titulo,
+        status:false
+    }
+    arrayTareas.push(tarea);
+    let ul=document.getElementById("tareas")
+        ul.innerHTML+=`<li><input type="checkbox" id="${ultimo}" onclick=Marcar(id)> ${titulo}</input></li>`
     ultimo++;
 }
 
 let TareasFinalizadas=(titulo)=> {
     let ul=document.getElementById("tareasFinalizadas")
-    ul.innerHTML+=`<li><input type="checkbox" id="${ultimo}" onclick=Marcar(id)> ${titulo}</input></li>`
+    ul.innerHTML+=`<li><input type="checkbox" id="${ultimo}" onclick=Marcar(${ultimo})> ${titulo}</input></li>`
     ultimo++;
 }
 
 let Marcar=(id)=>{
-    let tareaMarcar=arrayTareasIniciadas.find((tarea)=>tarea.id===id)
-    arrayTareasIniciadas.splice(id)
-    arrayTareasFinalizadas.push(tareaMarcar)
+    console.log(id)
+    arrayTareas[id].status=true;
+    Refrescar();
+}
+
+let Refrescar=()=>{
+    let ul=document.getElementById("tareas")
+    document.getElementById("tareas").innerHTML = "";
+    for(let i = 0; i<arrayTareas.length; i++){
+        if(arrayTareas[i].status){
+            ul.innerHTML+=`<li><input type="checkbox" id="${arrayTareas[i].id}" checked disabled="disabled"> <h10 style="color:red">${arrayTareas[i].titulo}</h10></input></li>`
+            
+        }else{
+            ul.innerHTML+=`<li><input type="checkbox" id="${arrayTareas[i].id}" onclick=Marcar(id)> <h10>${arrayTareas[i].titulo}</h10></input></li>`
+        }
+    }
 }
