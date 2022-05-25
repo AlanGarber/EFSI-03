6e5d04c2add7e5fe67e47b68ef5245b957f4ccf6
Santisiele <santi.siele@gmail.com>
2022-05-25 09:44:56 -0300

    extra y colores

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+
 let arrayTareas=[];
 let ultimo = 0;
 
@@ -9,26 +10,22 @@ let hideForm=()=>{
     document.getElementById("formAgregarTareas").style.visibility = "hidden";
 }
 
-let AgregarTarea=(titulo)=> {
+let AgregarTarea=(titulo, descripcion)=> {
     let tarea={
         id: ultimo,
         titulo:titulo,
+        descripcion: descripcion,
         status:false
     }
     arrayTareas.push(tarea);
-    let ul=document.getElementById("tareas")
-        ul.innerHTML+=`<li><input type="checkbox" id="${ultimo}" onclick=Marcar(id)> ${titulo}</input></li>`
-    ultimo++;
-}
-
-let TareasFinalizadas=(titulo)=> {
-    let ul=document.getElementById("tareasFinalizadas")
-    ul.innerHTML+=`<li><input type="checkbox" id="${ultimo}" onclick=Marcar(${ultimo})> ${titulo}</input></li>`
+    let ul=document.getElementById("tareas");
+        ul.innerHTML+=`<div class="ok">
+        <li><input type="checkbox" id="${ultimo}" onclick=Marcar(id)><h10> ${titulo} </h10><i class="fa-solid fa-eye fa-2xs" data-toggle="modal" data-target="#texto" onclick=Modal(${ultimo});></i></input></li>
+        </div>`
     ultimo++;
 }
 
 let Marcar=(id)=>{
-    console.log(id)
     arrayTareas[id].status=true;
     Refrescar();
 }
@@ -38,10 +35,42 @@ let Refrescar=()=>{
     document.getElementById("tareas").innerHTML = "";
     for(let i = 0; i<arrayTareas.length; i++){
         if(arrayTareas[i].status){
-            ul.innerHTML+=`<li><input type="checkbox" id="${arrayTareas[i].id}" checked disabled="disabled"> <h10 style="color:red">${arrayTareas[i].titulo}</h10></input></li>`
-            
+            ul.innerHTML+=`<div class="lista">
+            <li><input type="checkbox" id="${arrayTareas[i].id}" checked disabled="disabled"> <h10>${arrayTareas[i].titulo}</h10></input><i class="fa-solid fa-eye fa-2xs" data-toggle="modal" data-target="#texto onclick=Modal(${ultimo});></i> <i class="fa-solid fa-trash-can"></i></li>
+            </div>`
         }else{
-            ul.innerHTML+=`<li><input type="checkbox" id="${arrayTareas[i].id}" onclick=Marcar(id)> <h10>${arrayTareas[i].titulo}</h10></input></li>`
+            ul.innerHTML+=`<div class="ok">
+            <li><input type="checkbox" id="${arrayTareas[i].id}" onclick=Marcar(id); Modal(id)> <h10>${arrayTareas[i].titulo}</h10></input><i class="fa-solid fa-eye fa-2xs" data-toggle="modal" data-target="#texto onclick=Modal(${ultimo});></i> <i class="fa-solid fa-trash-can"></i></li>
+            </div>`
         }
+        
     }
+}
+
+let Modal=(id)=>{
+    document.getElementById("areaModalDescripcion").innerHTML=`<div class="modal fade" id="texto" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modals">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Descripcion tarea</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                </div>
+            </div>
+            <div class="modal-body">
+                <input type="text" class="form-control" id="tituloTarea" placeholder="${arrayTareas[id].titulo}" readonly><br>
+                <textarea name="textarea" rows="5" cols="25" class="form-control" id="descripcionTarea" placeholder="${arrayTareas[id].descripcion}"></textarea>
+            </div>
+            <div class="modal-footer">
+            <button type="button" data-dismiss="modal" class="btn btn-primary">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>`
+}
+
+let borrarTarea=(id)=>{
+
 }
